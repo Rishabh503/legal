b966f80731a99e1a60d5c432d254b673c34bca3f
Rishabh503 <rishabhtripathi2022@gmail.com>
2025-11-05 22:04:34 +0530

    added anagh code

--- src/lib/models/index.ts
+++ src/lib/models/index.ts
@@ -1,18 +1,23 @@
+
 // Import User first - it's the base model
 import User from './user';
 import Lawyer from './lawyer';
 import Client from './client';
 import Booking from './booking';
 import Review from './review';
+import Chat from './chat';
 
 // Export all models
-export { User, Lawyer, Client, Booking, Review };
+export { User, Lawyer, Client, Booking, Review, Chat };
 
 // Export default for easy import
-export default {
+const models = {
   User,
   Lawyer,
   Client,
   Booking,
-  Review
+  Review,
+  Chat
 };
+
+export default models;
